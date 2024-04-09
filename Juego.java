d9d1420cecb03fe5f660f194bca719ed40c42b7f
alexTorres2240 <166479415+alexTorres2240@users.noreply.github.com>
2024-04-09 08:15:40 -0700

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -2,6 +2,7 @@ package com.mycompany.cinquillo;
 import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Scanner;
 
 public class Juego {
     
@@ -15,7 +16,7 @@ public class Juego {
         mesa = new Mesa();
         baraja.generarCartas();
         for (int i=0; i<cantidadJugadores; i++){
-            jugadores.add(new Jugador("Jugador "+i));
+            jugadores.add(new Jugador("Jugador "+(i+1)));
         }
     }
     
@@ -69,47 +70,13 @@ public class Juego {
     }
     
     public static void main(String[] args){
-        Juego juego = new Juego(4);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cantidad de jugadores: ");
+        int cantidad = scanner.nextInt();
+        Juego juego = new Juego(cantidad);
         juego.repartirCartas();
-        JFrame jugador1 = new JFrame ("Jugador 1");
-        JFrame jugador2 = new JFrame ("Jugador 2");
-        JFrame jugador3 = new JFrame ("Jugador 3");
-        JFrame jugador4 = new JFrame ("Jugador 4");
-        JPanel panel1 = new JPanel(new FlowLayout());
-        JPanel panel2 = new JPanel(new FlowLayout());
-        JPanel panel3 = new JPanel(new FlowLayout());
-        JPanel panel4 = new JPanel(new FlowLayout());
-        for (int i = 0; i <= 9; i++) {
-            ImageIcon icono = new ImageIcon(juego.jugadores.get(0).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
-            JLabel etiqueta = new JLabel(icono);
-            panel1.add(etiqueta);
+        for (int i=0; i<cantidad; i++){
+            juego.jugadores.get(i).mostrarMano();
         }
-        jugador1.getContentPane().add(panel1);
-        jugador1.pack();
-        jugador1.setVisible(true);
-        for (int i = 0; i <= 9; i++) {
-            ImageIcon icono = new ImageIcon(juego.jugadores.get(1).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
-            JLabel etiqueta = new JLabel(icono);
-            panel2.add(etiqueta);   
-        }
-        jugador2.getContentPane().add(panel2);
-        jugador2.pack();
-        jugador2.setVisible(true);
-        for (int i = 0; i <= 9; i++) {
-            ImageIcon icono = new ImageIcon(juego.jugadores.get(2).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
-            JLabel etiqueta = new JLabel(icono);
-            panel3.add(etiqueta);
-        }
-        jugador3.getContentPane().add(panel3);
-        jugador3.pack();
-        jugador3.setVisible(true);
-        for (int i = 0; i <= 9; i++) {
-            ImageIcon icono = new ImageIcon(juego.jugadores.get(3).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
-            JLabel etiqueta = new JLabel(icono);
-            panel4.add(etiqueta);
-        }
-        jugador4.getContentPane().add(panel4);
-        jugador4.pack();
-        jugador4.setVisible(true);
     }
 }
