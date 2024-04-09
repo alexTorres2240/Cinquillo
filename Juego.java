0d9621e164308b2db202cd2435b59682df328118
alexTorres2240 <166479415+alexTorres2240@users.noreply.github.com>
2024-04-09 01:45:51 -0700

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -1,6 +1,7 @@
 package com.mycompany.cinquillo;
 import java.util.ArrayList;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
 public class Juego {
     
@@ -12,16 +13,14 @@ public class Juego {
         jugadores = new ArrayList<>();
         baraja = new Baraja();
         mesa = new Mesa();
-        Scanner scanner = new Scanner(System.in);
+        baraja.generarCartas();
         for (int i=0; i<cantidadJugadores; i++){
-            System.out.println("Ingresar nombre: ");
-            String nombre = scanner.next();
-            jugadores.add(new Jugador(nombre));
+            jugadores.add(new Jugador("Jugador "+i));
         }
         int index;
         switch (cantidadJugadores){
             case 2:
-                for (int i=0; i<22; i++){
+                for (int i=0; i<20; i++){
                     index = (int) (Math.random()*(baraja.getMazo().size()));
                     jugadores.get(0).getMano().add(baraja.getMazo().get(index));
                     baraja.getMazo().remove(index);
@@ -31,22 +30,83 @@ public class Juego {
                 }
                 break;
             case 3:
-                for (int i=0; i<16; i++){
+                for (int i=0; i<14; i++){
                     index = (int) (Math.random()*(baraja.getMazo().size()));
                     jugadores.get(0).getMano().add(baraja.getMazo().get(index));
                     baraja.getMazo().remove(index);
-                    if (i<15){
+                    if (i<13){
                         index = (int) (Math.random()*(baraja.getMazo().size()));
                         jugadores.get(1).getMano().add(baraja.getMazo().get(index));
                         baraja.getMazo().remove(index);
                     }
-                    if (i<15){
+                    if (i<13){
                         index = (int) (Math.random()*(baraja.getMazo().size()));
                         jugadores.get(2).getMano().add(baraja.getMazo().get(index));
                         baraja.getMazo().remove(index);
                     }
                 }
+                break;
             case 4:
+                for (int i=0; i<10; i++){
+                    index = (int) (Math.random()*(baraja.getMazo().size()));
+                    jugadores.get(0).getMano().add(baraja.getMazo().get(index));
+                    baraja.getMazo().remove(index);
+                    index = (int) (Math.random()*(baraja.getMazo().size()));
+                    jugadores.get(1).getMano().add(baraja.getMazo().get(index));
+                    baraja.getMazo().remove(index);
+                    index = (int) (Math.random()*(baraja.getMazo().size()));
+                    jugadores.get(2).getMano().add(baraja.getMazo().get(index));
+                    baraja.getMazo().remove(index);
+                    index = (int) (Math.random()*(baraja.getMazo().size()));
+                    jugadores.get(3).getMano().add(baraja.getMazo().get(index));
+                    baraja.getMazo().remove(index);
+                }
+                break;
         }
     }
+    
+    public static void main(String[] args){
+        Juego juego = new Juego(4);
+        JFrame jugador1 = new JFrame ("Jugador 1");
+        JFrame jugador2 = new JFrame ("Jugador 2");
+        JFrame jugador3 = new JFrame ("Jugador 3");
+        JFrame jugador4 = new JFrame ("Jugador 4");
+        JPanel panel1 = new JPanel(new FlowLayout());
+        JPanel panel2 = new JPanel(new FlowLayout());
+        JPanel panel3 = new JPanel(new FlowLayout());
+        JPanel panel4 = new JPanel(new FlowLayout());
+        for (int i = 0; i <= 9; i++) {
+            ImageIcon icono = new ImageIcon(juego.jugadores.get(0).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
+            JLabel etiqueta = new JLabel(icono);
+            panel1.add(etiqueta);
+        }
+        jugador1.getContentPane().add(panel1);
+        jugador1.pack();
+        jugador1.setVisible(true);
+        for (int i = 0; i <= 9; i++) {
+            ImageIcon icono = new ImageIcon(juego.jugadores.get(1).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
+            JLabel etiqueta = new JLabel(icono);
+            panel2.add(etiqueta);   
+        }
+        jugador2.getContentPane().add(panel2);
+        jugador2.pack();
+        jugador2.setVisible(true);
+        for (int i = 0; i <= 9; i++) {
+            ImageIcon icono = new ImageIcon(juego.jugadores.get(2).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
+            JLabel etiqueta = new JLabel(icono);
+            panel3.add(etiqueta);
+        }
+        jugador3.getContentPane().add(panel3);
+        jugador3.pack();
+        jugador3.setVisible(true);
+        for (int i = 0; i <= 9; i++) {
+            ImageIcon icono = new ImageIcon(juego.jugadores.get(3).getMano().get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
+            JLabel etiqueta = new JLabel(icono);
+            panel4.add(etiqueta);
+        }
+        jugador4.getContentPane().add(panel4);
+        jugador4.pack();
+        jugador4.setVisible(true);
+    }
 }
+
