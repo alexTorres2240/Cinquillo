c104c90f496891ea3591c4b648121040669d26a2
alexTorres2240 <166479415+alexTorres2240@users.noreply.github.com>
2024-04-09 08:14:58 -0700

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,5 +1,11 @@
 package com.mycompany.cinquillo;
+import java.awt.FlowLayout;
+import java.awt.Image;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Jugador {
     
@@ -31,5 +37,18 @@ public class Jugador {
     
     public ArrayList<Carta> getMano(){
         return this.mano;
+    }
+    
+    public void mostrarMano(){
+        JFrame frame = new JFrame (this.nombre);
+        JPanel panel = new JPanel(new FlowLayout());
+        for (int i=0; i<this.mano.size(); i++) {
+            ImageIcon icono = new ImageIcon(this.mano.get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
+            JLabel etiqueta = new JLabel(icono);
+            panel.add(etiqueta);
+        }
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
