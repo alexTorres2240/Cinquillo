958b5b973b932db5576a465a72fea1d43774cf14
alexTorres2240 <166479415+alexTorres2240@users.noreply.github.com>
2024-04-10 00:27:55 -0700

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -75,8 +75,65 @@ public class Juego {
         int cantidad = scanner.nextInt();
         Juego juego = new Juego(cantidad);
         juego.repartirCartas();
+        JPanel panelPrincipal = new JPanel(null);
+        JPanel panelSecundario;
+        JFrame ventanaJuego = new JFrame("Juego");
+        panelPrincipal.setBackground(new Color(53, 148, 100));
         for (int i=0; i<cantidad; i++){
-            juego.jugadores.get(i).mostrarMano();
+            panelSecundario = juego.jugadores.get(i).getPanelMano();
+            if (juego.jugadores.size() == 2){
+                switch (i){
+                    case 0:
+                        panelSecundario.setBounds(100, 620, 1160, 80);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                    case 1:
+                        panelSecundario.setBounds(100, 0, 1160, 80);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                }
+            }else if(juego.jugadores.size() == 3){
+                switch (i){
+                    case 0:
+                        panelSecundario.setBounds(450, 520, 464, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                    case 1:
+                        panelSecundario.setBounds(50, 0, 464, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                    case 2:
+                        panelSecundario.setBounds(825, 0, 464, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                }
+            }else{
+                switch (i){
+                    case 0:
+                        panelSecundario.setBounds(475, 520, 320, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                    case 1:
+                        panelSecundario.setBounds(25, 260, 320, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                    case 2:
+                        panelSecundario.setBounds(475, 0, 320, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                    case 3:
+                        panelSecundario.setBounds(1025, 260, 320, 160);
+                        panelPrincipal.add(panelSecundario);
+                        break;
+                }
+            }
         }
+        ventanaJuego.getContentPane().add(panelPrincipal);
+        ventanaJuego.pack();
+        ventanaJuego.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        ventanaJuego.setLocationRelativeTo(null);
+        ventanaJuego.setVisible(true);
+        ventanaJuego.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    
 }
