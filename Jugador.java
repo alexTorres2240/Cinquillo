66ba883f69e3270ee3c3363fd204d1011d6e4c63
alexTorres2240 <166479415+alexTorres2240@users.noreply.github.com>
2024-04-10 00:27:12 -0700

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -3,9 +3,8 @@ import java.awt.FlowLayout;
 import java.awt.Image;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class Jugador {
     
@@ -34,21 +33,21 @@ public class Jugador {
     public int puntos(){
         return this.puntos;
     }
-    
-    public ArrayList<Carta> getMano(){
+
+    public ArrayList<Carta> getMano() {
         return this.mano;
     }
     
-    public void mostrarMano(){
-        JFrame frame = new JFrame (this.nombre);
+    public JPanel getPanelMano(){
         JPanel panel = new JPanel(new FlowLayout());
         for (int i=0; i<this.mano.size(); i++) {
             ImageIcon icono = new ImageIcon(this.mano.get(i).getIcono().getImage().getScaledInstance(54, 72, Image.SCALE_DEFAULT));
-            JLabel etiqueta = new JLabel(icono);
+            JButton etiqueta = new JButton(icono);
+            etiqueta.setPreferredSize(new Dimension(58, 74));
+            panel.setBackground(new Color(53, 148, 100));
             panel.add(etiqueta);
         }
-        frame.getContentPane().add(panel);
-        frame.pack();
-        frame.setVisible(true);
+        return panel;
     }
+   
 }
