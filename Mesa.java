8532bf61fcbbc91396a2810c8a54d88faea5d82d
alexTorres2240 <166479415+alexTorres2240@users.noreply.github.com>
2024-04-11 14:41:22 -0700

    Update Mesa.java

--- Mesa.java
+++ Mesa.java
@@ -39,66 +39,95 @@ public class Mesa {
     public void llenarMesa(){
         for (int i=1; i<11; i++){
             this.escaleraBastos.add(new Carta("bastos", i));
+            this.escaleraBastos.get(i-1).setVisible(false);
             this.escaleraCopas.add(new Carta("copas", i));
+            this.escaleraCopas.get(i-1).setVisible(false);
             this.escaleraEspadas.add(new Carta("espadas", i));
+            this.escaleraEspadas.get(i-1).setVisible(false);
             this.escaleraOros.add(new Carta("oros", i));
+            this.escaleraOros.get(i-1).setVisible(false);
             
+        }
+    }
+    
+    public void jugarCarta(Carta carta){
+        switch (carta.getFigura()){
+            case "bastos":
+                this.escaleraBastos.get(carta.getValor()-1).setVisible(true);
+                break;
+            case "copas":
+                this.escaleraCopas.get(carta.getValor()-1).setVisible(true);
+                break;
+            case "espadas":
+                this.escaleraEspadas.get(carta.getValor()-1).setVisible(true);
+                break;
+            case "oros":
+                this.escaleraOros.get(carta.getValor()-1).setVisible(true);
+                break;
         }
     }
     
     public JPanel getPanelBastos(){
         JPanel panel = new JPanel();
+        panel.setVisible(false);
         for (int i=0; i<this.escaleraBastos.size(); i++){
             ImageIcon icono = new ImageIcon(this.escaleraBastos.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
-            JButton etiqueta = new JButton(icono);
+            JLabel etiqueta = new JLabel(icono);
             etiqueta.setPreferredSize(new Dimension(36, 48));
             panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
+            etiqueta.setVisible(true);
             panel.add(etiqueta);
-            panel.setVisible(false);
         }
+        panel.setVisible(true);
         return panel;
     }
     
     public JPanel getPanelCopas(){
         JPanel panel = new JPanel();
+        panel.setVisible(false);
         for (int i=0; i<this.escaleraCopas.size(); i++){
             ImageIcon icono = new ImageIcon(this.escaleraCopas.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
-            JButton etiqueta = new JButton(icono);
+            JLabel etiqueta = new JLabel(icono);
             etiqueta.setPreferredSize(new Dimension(36, 48));
             panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
+            etiqueta.setVisible(true);
             panel.add(etiqueta);
-            panel.setVisible(false);
         }
+        panel.setVisible(true);
         return panel;
     }
     
     public JPanel getPanelEspadas(){
         JPanel panel = new JPanel();
+        panel.setVisible(false);
         for (int i=0; i<this.escaleraEspadas.size(); i++){
             ImageIcon icono = new ImageIcon(this.escaleraEspadas.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
-            JButton etiqueta = new JButton(icono);
+            JLabel etiqueta = new JLabel(icono);
             etiqueta.setPreferredSize(new Dimension(36, 48));
             panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
+            etiqueta.setVisible(true);
             panel.add(etiqueta);
-            panel.setVisible(false);
         }
+        panel.setVisible(true);
         return panel;
     }
     
     public JPanel getPanelOros(){
         JPanel panel = new JPanel();
+        panel.setVisible(false);
         for (int i=0; i<this.escaleraOros.size(); i++){
             ImageIcon icono = new ImageIcon(this.escaleraOros.get(i).getIcono().getImage().getScaledInstance(36, 48, Image.SCALE_DEFAULT));
-            JButton etiqueta = new JButton(icono);
+            JLabel etiqueta = new JLabel(icono);
             etiqueta.setPreferredSize(new Dimension(36, 48));
             panel.setBackground(new Color(53, 148, 100));
             etiqueta.setBounds(100, 100, 36, 48);
+            etiqueta.setVisible(true);
             panel.add(etiqueta);
-            panel.setVisible(false);
         }
+        panel.setVisible(true);
         return panel;
     }
     
